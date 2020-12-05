1edaf3662c7d14066fc2681d8a3e9a0f82914a94
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-05 16:29:48 +0200

    Add fragment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import '../App.css';
+import { Fragment } from 'react';
 
 const App = () => (
-  <div className="App">
+  <Fragment className="App">
     <header className="App-header">
       <h1>
         Welcome to calculator developed with react
       </h1>
     </header>
-  </div>
+  </Fragment>
 );
 
 export default App;
