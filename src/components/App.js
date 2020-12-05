1b797fb8184605b66d9fb9d7531fde6998b61759
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-05 16:25:34 +0200

    Create component folder and add App.js in it

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>
+        Welcome to calculator developed with react
+      </h1>
+    </header>
+  </div>
+);
+
+export default App;
