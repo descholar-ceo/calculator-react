30b7a23add211fe1a2f717290876238bd264b1ba
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-05 19:14:56 +0200

    Add classnames

--- src/helpers/mark-helpers.js
+++ src/helpers/mark-helpers.js
@@ -6,4 +6,6 @@ export const groupThree = () => (['4', '5', '6', '-']);
 export const groupFour = () => (['1', '2', '3', '+']);
 export const groupFive = () => (['0', '.', '=']);
 export const mapMarksToPanel = arr => arr.map(curr => (<Button key={curr} btnMark={curr} />));
-export const btnPanel = rows => rows.map(curr => (<div key={curr}>{ curr }</div>));
+export const btnPanel = rows => rows.map(
+  curr => (<div key={curr} className={curr[0]}>{ curr }</div>),
+);
