fc6d7cad8ed5970e4a5a76472096d585568c660a
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-06 16:20:24 +0200

    Add AC, Mod, percentage logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,26 @@
-const calculate = (calculatorData, btnName) => {};
+const calculate = (calculatorData, btnName) => {
+  let { total, next, operation } = calculatorData;
+  const symbols = ['+', '-', 'x', '÷', '%'];
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+  if (btnName === '+/-') {
+    const res = total * -1;
+    total = res.toString();
+    if (next) {
+      const res = next * -1;
+      next = res.toString();
+    }
+  } else if (btnName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (btnName === '%' && total) {
+    const res = total / 100;
+    total = res.toString();
+    operation = null;
+  }
+
+  return { total, next, operation };
+};
 
 export default calculate;
