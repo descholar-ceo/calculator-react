9e91b85774b78823ad76e03f0639a6ac051d5d15
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-07 12:20:40 +0200

    Implement simple math in operate

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,36 @@
 import Big from 'big.js';
 
-const operate = () => {};
+const operate = (numberOne, numberTwo, operation) => {
+  let res;
+  const firstArg = new Big(numberOne);
+  const secondArg = new Big(numberTwo);
+
+  switch (operation) {
+    case '-':
+      res = firstArg.minus(secondArg);
+      break;
+    case '÷':
+      if (secondArg !== 0) {
+        res = firstArg.div(secondArg);
+      } else {
+        res = 'Math Error';
+      }
+      break;
+    case 'x':
+      res = firstArg.mul;
+      break;
+    case '+':
+      res = firstArg.plus(secondArg);
+      break;
+    case '%':
+      res = firstArg.div(100);
+      break;
+    default:
+      res = 0;
+  }
+  const resultToReturn = res.toString();
+
+  return resultToReturn;
+};
 
 export default operate;
