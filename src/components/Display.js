24d47e1be32b5f45fd44f7c552a4298fad177ff8
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-07 16:58:35 +0200

    Pass btn name

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => <h1 className="result-display">{ result }</h1>;
+const Display = ({ result, next }) => <h1 className="result-display">{ result || next }</h1>;
 
 Display.propTypes = {
   result: PropTypes.string,
+  next: PropTypes.string,
 };
 
 Display.defaultProps = {
   result: '0',
+  next: null,
 };
 
 export default Display;
