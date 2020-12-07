52652edae2035d14467996e91958d694bd2990d0
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-07 09:52:28 +0200

    Add +-, percentage and AC symbols

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,23 +1,23 @@
 const calculate = (calculatorData, btnName) => {
   let { total, next, operation } = calculatorData;
-  const symbols = ['+', '-', 'x', '÷', '%'];
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const symbolsArr = ['+', '-', 'x', '÷', '%'];
+  const allNumbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
-  if (btnName === '+/-') {
+  if (btnName === '%' && total) {
+    const res = total / 100;
+    total = res.toString();
+    operation = null;
+  } else if (btnName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (btnName === '+/-') {
     const res = total * -1;
     total = res.toString();
     if (next) {
       const res = next * -1;
       next = res.toString();
     }
-  } else if (btnName === 'AC') {
-    total = null;
-    next = null;
-    operation = null;
-  } else if (btnName === '%' && total) {
-    const res = total / 100;
-    total = res.toString();
-    operation = null;
   }
 
   return { total, next, operation };
