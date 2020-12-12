187fd820b14d6ed257a733391041d0a5e7d1951f
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-12 17:33:28 +0200

    Create calculator element content

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,7 @@
-import { useState } from 'react';
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
-import calculate from '../logic/calculate';
+
 
 const App = () => {
-  const [state, setState] = useState({ total: null, next: null, operation: null });
-  const { total, next, operation } = state;
-  const handleClick = btnName => {
-    setState(calculate({ total, next, operation }, btnName));
-  };
-  return (
-    <>
-      <Display result={next || total || '0'} />
-      <ButtonPanel clickHandler={handleClick} />
-    </>
-  );
+  
 };
 
 export default App;
---
+++ src/pages/calculator.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import ButtonPanel from '../components/ButtonPanel';
+import Display from '../components/Display';
+import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const { total, next, operation } = state;
+  const handleClick = btnName => {
+    setState(calculate({ total, next, operation }, btnName));
+  };
+  return (
+    <>
+      <Display result={next || total || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
+
+export default Calculator;
