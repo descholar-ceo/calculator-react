836bbe8bb63e90d98f92e73314cf26e0a46de751
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-12 18:19:38 +0200

    Create fourofourpage

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 import Calculator from '../pages/calculator';
 import Home from '../pages/home';
 import Quotes from '../pages/quotes';
+import Fourofourpage from '../pages/fourofourpage';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/calculator" component={Calculator} />
     <Route exact path="/quotes" component={Quotes} />
+    <Route component={Fourofourpage} />
   </Switch>
 );
 
