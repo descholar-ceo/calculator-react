219d19fcdfd4e51cd8f0e114c3063489339a5116
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-14 10:30:07 +0200

    Add AC test

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -7,4 +7,26 @@ describe('Calculate function tests', () => {
     expect(result.total).toEqual(0.23);
     done();
   });
+
+  it('When passed a AC, it returns a nulled result', done => {
+    const result = calculate({ total: 23, next: 45, operation: '+' }, 'AC');
+    expect(result).toHaveProperty('total');
+    expect(result).toHaveProperty('next');
+    expect(result).toHaveProperty('operation');
+    expect(result.total).toEqual(null);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+    done();
+  });
+
+  it('When passed a AC, it returns a nulled result', done => {
+    const result = calculate({ total: 23, next: 45, operation: '+' }, 'AC');
+    expect(result).toHaveProperty('total');
+    expect(result).toHaveProperty('next');
+    expect(result).toHaveProperty('operation');
+    expect(result.total).toEqual(null);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+    done();
+  });
 });
