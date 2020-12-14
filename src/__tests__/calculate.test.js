4b3901e8973bd80f40dd3fada352fa3d78fdb665
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-14 10:46:29 +0200

    Add tests for btn 2

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -19,10 +19,38 @@ describe('Calculate function tests', () => {
     done();
   });
 
-  it('When passed a AC, it returns a nulled result', done => {
+  it('When passed a +/-, it returns a negative result or a positive result depending on the passed value', done => {
     const result = calculate({ total: 23, next: null, operation: '+' }, '+/-');
     expect(result).toHaveProperty('total');
     expect(result.total).toEqual(-23);
+    done();
+  });
+
+  it('When a btn of 0 is clicked, and total is null, operation is null and next is null, it returns a total = "0"', done => {
+    const result = calculate({ total: null, next: null, operation: null }, '0');
+    expect(result).toHaveProperty('total');
+    expect(result.total).toEqual('0');
+    done();
+  });
+
+  it('When a btn of 0 is clicked, and total is not null, operation is null and next is null, it returns a total = "0"', done => {
+    const result = calculate({ total: 1, next: null, operation: null }, '0');
+    expect(result).toHaveProperty('total');
+    expect(result.total).toEqual('10');
+    done();
+  });
+
+  it('When a btn of 1 is clicked, and total is not null, operation is not null and next is null, it returns a next = "0"', done => {
+    const result = calculate({ total: 1, next: null, operation: '+' }, '1');
+    expect(result).toHaveProperty('next');
+    expect(result.next).toEqual('1');
+    done();
+  });
+
+  it('When a btn of 2 is clicked, and total is not null, operation is not null and next is null, it returns a next = "0"', done => {
+    const result = calculate({ total: 1, next: 1, operation: '+' }, '2');
+    expect(result).toHaveProperty('next');
+    expect(result.next).toEqual('12');
     done();
   });
 });
